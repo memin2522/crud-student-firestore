ce8d808a780ea08780ddcea7fcc8e64c3c6268cb
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-04-08 20:48:28 -0500

    midificador

--- modules/firebase_init.js
+++ modules/firebase_init.js
@@ -7,13 +7,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwTMj3lxsSFWX4UDxP_si5GppllWkGHYA",
-  authDomain: "classbdd.firebaseapp.com",
-  projectId: "classbdd",
-  storageBucket: "classbdd.firebasestorage.app",
-  messagingSenderId: "717794050840",
-  appId: "1:717794050840:web:6292fc64cfcf5f02fc546d",
-  measurementId: "G-4QFXNF6WZ8"
+  apiKey: "AIzaSyBBgoWaCd6APngiq3Jv-d4abmByqZyFxJ8",
+  authDomain: "db-multimedia.firebaseapp.com",
+  projectId: "db-multimedia",
+  storageBucket: "db-multimedia.firebasestorage.app",
+  messagingSenderId: "703694156223",
+  appId: "1:703694156223:web:cd702f775e4417841db10a"
 };
 
 
