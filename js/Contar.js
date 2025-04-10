405056cd8ad85a1c4dc15649b46e2fa5a8d5c611
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-04-10 13:09:16 -0500

    guardar cambios

--- js/Contar.js
+++ js/Contar.js
@@ -15,9 +15,9 @@ const displayResults = (docs) => {
     docs.forEach(doc => {
         const infoImportant = `
         <div class="card2">
-        <h4> Expiry Date: ${doc.ExpiryDate} </h4>
-        <h4> User card: ${doc.NameUser} </h4>
-        <h4> Number Card: ${doc.NumberCard} </h4>
+        <h4> Expiry Date: ${doc.expiryDate} </h4>
+        <h4> User card: ${doc.nameUser} </h4>
+        <h4> Number Card: ${doc.numberCard} </h4>
         </div>
         `;
         resultsEl.innerHTML += infoImportant;
@@ -36,4 +36,18 @@ document.getElementById("OrderCVV").addEventListener("click", async () => {
     displayResults(docs);
 });
 
+
+document.getElementById("orderLimitQueryBtn").addEventListener("click", async () => {
+    const column = document.getElementById("orderColumn").value.trim();
+    const direction = document.getElementById("orderDir").value;
+    const limitNum = parseInt(document.getElementById("limitNum").value, 10);
+
+    const docs = await firestore.combinedQuery([], { column, direction }, limitNum);
+    docs.forEach(doc => {
+        for (const key in doc) {
+            doc[key] = isNaN(doc[key]) ? doc[key] : Number(doc[key]);
+        }
+    });
+    displayResults(docs);
+});
 
