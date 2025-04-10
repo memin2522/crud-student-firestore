04178bd57b6e3623b848fa7275b11597ce2ec301
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-04-10 14:34:45 -0500

    prototipado

--- js/Contar.js
+++ js/Contar.js
@@ -9,7 +9,6 @@ const displayResults = (docs) => {
         resultsEl.textContent = "No documents found.";
         return;
     }
-    // Limpiar contenido anterior
     resultsEl.innerHTML = "";
 
     docs.forEach(doc => {
@@ -50,4 +49,59 @@ document.getElementById("orderLimitQueryBtn").addEventListener("click", async ()
     });
     displayResults(docs);
 });
+
+document.getElementById("prefixQueryBtn").addEventListener("click", async () => {
+    const column = document.getElementById("prefixColumn").value.trim();
+    const prefix = document.getElementById("prefixValue").value.trim();
+
+    const docs = await firestore.prefixSearch(column, prefix);
+    displayResults(docs);
+});
+
+
+        // Add fields to filter
+        document.getElementById("addFilterBtn").addEventListener("click", () => {
+            console.log("Add field clicked")
+            const container = document.getElementById("filtersContainer");
+            const div = document.createElement("div");
+            div.classList.add("filter");
+
+            div.innerHTML = `
+                <input type="text" placeholder="Column" class="ContainerInput filterColumn">
+                <input type="text" placeholder="Comparator (==, >, <, etc)" class="ContainerInput filterComparator">
+                <input type="text" placeholder="Value" class="ContainerInput filterValue">
+                `;
+
+            container.appendChild(div);
+        });
+
+        // Multi query
+        document.getElementById("multiWhereQueryBtn").addEventListener("click", async () => {
+            const filterDivs = document.querySelectorAll("#filtersContainer .filter");
+            const filters = [];
+
+            filterDivs.forEach(div => {
+            const column = div.querySelector(".filterColumn").value.trim();
+            const comparison = div.querySelector(".filterComparator").value.trim();
+            const valueRaw = div.querySelector(".filterValue").value.trim();
+
+            if (column && comparison) {
+                let value;
+                if (!isNaN(valueRaw) && valueRaw !== "") {
+                value = Number(valueRaw);
+                } else {
+                value = valueRaw;
+                }
+                filters.push({ column, comparison, value });
+
+                
+            }
+
+            console.log(`Filter added: ${column} ${comparison} ${value}`);
+            });
+
+
+            const docs = await firestore.combinedQuery(filters);
+            displayResults(docs);
+        });
 
