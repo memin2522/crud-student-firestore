606a6a983520fbc9baaf35bd23d00be9c63760db
sebas64mil <162932132+sebas64mil@users.noreply.github.com>
2025-04-10 13:25:41 -0500

    cambios

--- modules/firebase_init.js
+++ modules/firebase_init.js
@@ -5,7 +5,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
-/*
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBgoWaCd6APngiq3Jv-d4abmByqZyFxJ8",
   authDomain: "db-multimedia.firebaseapp.com",
@@ -14,9 +14,9 @@ const firebaseConfig = {
   messagingSenderId: "703694156223",
   appId: "1:703694156223:web:cd702f775e4417841db10a"
 };
-*/
 
 
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyDwTMj3lxsSFWX4UDxP_si5GppllWkGHYA",
   authDomain: "classbdd.firebaseapp.com",
@@ -26,7 +26,7 @@ const firebaseConfig = {
   appId: "1:717794050840:web:6292fc64cfcf5f02fc546d",
   measurementId: "G-4QFXNF6WZ8"
  };
-
+*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
